3074798c9318465a8c8a8c86fbfc14178687a1a7
MyznEiji <em711th@icloud.com>
2018-05-14 17:54:10 +0900

    add : hoge

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpackの設定ファイル
+
+var publidDir = __dirname + '/public';
+module.exports = {
+  // 元となるファイル(コンパイルする)
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    // outputはどこに吐き出すか
+    path: publidDir,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+        rules: [{
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015'],
+          },
+        }],
+      },
+  resolve: {
+    // entryからインポートされたライブラリを検索する拡張子を指定している
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    // webpackDevServerの設定
+    historyApiFallback: true,
+    // contentBaseがドキュメントルートになる
+    contentBase: publidDir
+  }
+};
