0b595c2993d1ea3f286295763e64db3da22fbeaf
MyznEiji <em711th@icloud.com>
2018-05-15 18:14:10 +0900

    component

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+// 
+// function App(props){
+//   return (<div>Hello From Functional App</div>);
+// }
+
+class App extends Component{
+  render(){
+      return (<div>Hello From Class App </div>);
+  }
+}
+
+
+export default App;
