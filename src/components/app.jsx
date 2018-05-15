2d550ae0751c519a8e5d36ec4085bc67375f2186
MyznEiji <em711th@icloud.com>
2018-05-15 18:32:34 +0900

    add : props

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
-// 
+
+import Greeting from './greeting';
+//
 // function App(props){
 //   return (<div>Hello From Functional App</div>);
 // }
 
 class App extends Component{
   render(){
-      return (<div>Hello From Class App </div>);
+      // return (<div>Hello From Class App </div>);
+    return (
+      <div>
+        <Greeting name="Bob" />
+        <Greeting name="John" />
+      </div>
+    );
   }
 }
 
