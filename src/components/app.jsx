752ea290a63d22edaff06038abf85ed785e0181b
MyznEiji <em711th@icloud.com>
2018-05-15 19:10:54 +0900

    add : dom react

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,12 +7,35 @@ import Greeting from './greeting';
 // }
 
 class App extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      name: "Hoge",
+    };
+  }
+
+  //eはdomのイベントを取得している、
+  handlNameChange(name)
+  {
+    // target.valueはinputで入力された文字を取得
+
+    this.setState({ name });
+  }
+
+
   render(){
       // return (<div>Hello From Class App </div>);
     return (
       <div>
-        <Greeting name="Bob" />
-        <Greeting name="John" />
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handlNameChange(e.target.value)}
+        />
+        <button onClick={() => this.handlNameChange('Bob')}>I am Bob</button>
+        <Greeting name={this.state.name } />
+
       </div>
     );
   }
