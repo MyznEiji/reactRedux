b90eb2d2a1025bc33e4b997a3ff0e929e8403634
MyznEiji <em711th@icloud.com>
2018-05-17 19:21:34 +0900

    case error

--- longitudeLatitude/src/components/app.jsx
+++ longitudeLatitude/src/components/app.jsx
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import SearchForm from './searchForm';
 import GeocodeResult from './GeocodeResult';
 
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json'
 // function App(props){
 //   return (<div>Hello From Functional App</div>);
 // }
@@ -16,9 +18,46 @@ class App extends Component{
   }
 
 
+  setErrorMessage(message){
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
+
   handlePlaceSubmit(place)
   {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, {params: {address: place} })
+      .then((results) => {
+        console.log(results);
+        const data = results.data;
+        const result = data.results[0];
+        switch(data.status){
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりません');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました');
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('通信に失敗しました');
+      });
+
   }
 
   render(){
