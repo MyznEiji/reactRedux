aa27f145b58b4f29587e1eb9ba9632d7efcabbcc
MyznEiji <em711th@icloud.com>
2018-05-22 17:45:52 +0900

    fine : longitudeLatitude

--- longitudeLatitude/src/components/app.jsx
+++ longitudeLatitude/src/components/app.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 import SearchForm from './searchForm';
 import GeocodeResult from './GeocodeResult';
+import Map from './Map';
 
 const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json'
 // function App(props){
@@ -71,6 +72,7 @@ class App extends Component{
           lat={this.state.lat}
           lng={this.state.lng}
         />
+        <Map lat={this.state.lat}  lng={this.state.lng}/>
       </div>
     );
   }
